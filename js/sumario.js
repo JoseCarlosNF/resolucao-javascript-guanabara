c8112300a010584dd91d426bff5f20b7b174749e
cassio8186 <cassio8186@gmail.com>
2020-04-13 11:23:57 -0300

    Corrige caminhos criados pelo javascript.
    ![Lista de desafios](https://i.imgur.com/BdzPQXv.png)
    
    Ao entrar em um desses desafios o site direciona para o diretório html onde estão contidos todos os desafios.
    
    Porém ao entrar em um desses desafios o link direciona novamente para outro diretório html, o qual não existe.
    
    ![link direcionando para outro diretório html](https://i.imgur.com/XBjsRvu.png)
    
    Foi criado uma função que envolve o método que gera a lista onde nesta função foi adicionado um parâmetro para por o diretório em que deseja direcionar o link.
    
    Após isso o js faz a verificação se já está no diretório html, caso esteja a função vai direcionar para a mesma página.

--- js/sumario.js
+++ js/sumario.js
@@ -1,20 +1,37 @@
 const resp = window.document.getElementById("UL");
 
 const desafios_obj = {
-  d001: "Desafio das Mensagens",
-  d002: "Boas-vindas",
-  d003: "Antes e Depois",
-  d004: "Calculando o Troco",
-  d005: "Conversor de Medidas",
-  d006: "Conversor de Temperaturas",
-  d007: "Conversor de BRL 🠞 USD",
-  d008: "Desconto de 10%",
-  d009: "Reajuste Salarial",
-  d010: "Resolvendo de Bhaskara",
-  d011: "Ano Bissexto",
-  d012: "Preço antes, preço depois",
+	d001: "Desafio das Mensagens",
+	d002: "Boas-vindas",
+	d003: "Antes e Depois",
+	d004: "Calculando o Troco",
+	d005: "Conversor de Medidas",
+	d006: "Conversor de Temperaturas",
+	d007: "Conversor de BRL 🠞 USD",
+	d008: "Desconto de 10%",
+	d009: "Reajuste Salarial",
+	d010: "Resolvendo de Bhaskara",
+	d011: "Ano Bissexto",
+	d012: "Preço antes, preço depois",
 };
 
-Object.keys(desafios_obj).map((element) => {
-  resp.innerHTML += `<li><a href="./html/${element}.html"><b>${element}</b> - ${desafios_obj[element]}</a></li>`;
-});
+/**
+ *  @function criaListaDesafios -
+ * @param {String} diretorio a qual a página será direcionada.
+ */
+const criaListaDesafios = (diretorio) => {
+	Object.keys(desafios_obj).map((element) => {
+		resp.innerHTML += `<li><a href=".${
+			diretorio == undefined ? "" : diretorio
+		}/${element}.html"><b>${element}</b> - ${desafios_obj[element]}</a></li>`;
+	});
+};
+
+if (window.location.pathname.includes("/html/")) {
+	// Como já está na pagina html direciona para o mesmo diretorio.
+	criaListaDesafios();
+	console.log("Estou em html");
+} else {
+	// Direciona para HTML
+	criaListaDesafios("/html");
+}
